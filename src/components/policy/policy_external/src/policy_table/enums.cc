77fa25cbd91b3c608d6aa7a2e815b6dea9fbc6a0
Ira Lytvynenko <ILytvynenko@luxoft.com>
2018-02-27 12:55:10 +0200

    New RC module SEAT

--- src/components/policy/policy_external/src/policy_table/enums.cc
+++ src/components/policy/policy_external/src/policy_table/enums.cc
@@ -735,6 +735,8 @@ bool IsValidEnum(ModuleType val) {
       return true;
     case MT_RADIO:
       return true;
+    case MT_SEAT:
+      return true;
     default:
       return false;
   }
@@ -745,6 +747,8 @@ const char* EnumToJsonString(ModuleType val) {
       return "CLIMATE";
     case MT_RADIO:
       return "RADIO";
+    case MT_SEAT:
+      return "SEAT";
     default:
       return "";
   }
@@ -756,6 +760,9 @@ bool EnumFromJsonString(const std::string& literal, ModuleType* result) {
     return true;
   } else if ("RADIO" == literal) {
     *result = MT_RADIO;
+    return true;
+  } else if ("SEAT" == literal) {
+    *result = MT_SEAT;
     return true;
   } else {
     return false;
