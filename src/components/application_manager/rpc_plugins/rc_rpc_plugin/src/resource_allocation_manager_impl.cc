c8e39404819355d48b659d47b5b82cbeaea02d6d
Ira Lytvynenko (GitHub) <ILytvynenko@luxoft.com>
2018-05-21 12:02:51 +0300

    Fix behavior in case when RC functionality is disabled from HMI

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/resource_allocation_manager_impl.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/resource_allocation_manager_impl.cc
@@ -23,7 +23,8 @@ ResourceAllocationManagerImpl::ResourceAllocationManagerImpl(
     application_manager::rpc_service::RPCService& rpc_service)
     : current_access_mode_(hmi_apis::Common_RCAccessMode::AUTO_ALLOW)
     , app_mngr_(app_mngr)
-    , rpc_service_(rpc_service) {}
+    , rpc_service_(rpc_service)
+    , is_rc_enabled_(true) {}
 
 ResourceAllocationManagerImpl::~ResourceAllocationManagerImpl() {}
 
@@ -206,6 +207,14 @@ void ResourceAllocationManagerImpl::SetResourceFree(
   }
   allocated_resources_.erase(allocation);
   LOG4CXX_DEBUG(logger_, "Resource " << module_type << " is released.");
+}
+
+bool ResourceAllocationManagerImpl::is_rc_enabled() const {
+  return is_rc_enabled_;
+}
+
+void ResourceAllocationManagerImpl::set_rc_enabled(const bool value) {
+  is_rc_enabled_ = value;
 }
 
 std::vector<std::string> ResourceAllocationManagerImpl::GetAcquiredResources(
